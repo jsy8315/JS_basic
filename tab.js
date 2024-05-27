63240f57b49fecd59c1f8e9720b8b5ddfe6b5809
jsy8315 <tdhitbtd@gmail.com>
2024-05-27 21:22:22 +0900

    02.10.탭기능 만들며 배우는 for 반복문02_for반복문 및 탭 증가시 동작하는 코드 구현

--- tab.js
+++ tab.js
@@ -1,3 +1,7 @@
+for (var i = 0; i < 3; i++) {
+    console.log(i);
+}
+
 // 버튼0 누르면 
 // - 모든 버튼에 붙은 orange 클래스명 제거
 // - 버튼0에 orange 클래스명 추가
@@ -7,24 +11,28 @@
 // 좋은 관습 : 자주 쓰는 셀렉터 변수에 넣어쓰기
 var 버튼 = $('.tab-button');
 var 컨텐트 = $('.tab-content');
+var countTab = $('.tab-content').length;
 
-버튼.eq(0).on('click', function() {
-    버튼.removeClass('orange');
-    버튼.eq(0).addClass('orange');
-    컨텐트.removeClass('show');
-    컨텐트.eq(0).addClass('show');
-})
+// for 반복문으로 코드 복붙 
+for (let i = 0; i < countTab; i++) {
+    버튼.eq(i).on('click', function() {
+        버튼.removeClass('orange');
+        버튼.eq(i).addClass('orange');
+        컨텐트.removeClass('show');
+        컨텐트.eq(i).addClass('show');
+    })
+}
 
-버튼.eq(1).on('click', function() {
-    버튼.removeClass('orange');
-    버튼.eq(1).addClass('orange');
-    컨텐트.removeClass('show');
-    컨텐트.eq(1).addClass('show');
-})
+// 버튼.eq(1).on('click', function() {
+//     버튼.removeClass('orange');
+//     버튼.eq(1).addClass('orange');
+//     컨텐트.removeClass('show');
+//     컨텐트.eq(1).addClass('show');
+// })
 
-버튼.eq(2).on('click', function() {
-    버튼.removeClass('orange');
-    버튼.eq(2).addClass('orange');
-    컨텐트.removeClass('show');
-    컨텐트.eq(2).addClass('show');
-})
+// 버튼.eq(2).on('click', function() {
+//     버튼.removeClass('orange');
+//     버튼.eq(2).addClass('orange');
+//     컨텐트.removeClass('show');
+//     컨텐트.eq(2).addClass('show');
+// })
