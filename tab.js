044b10f68f4798a7c14a99eb011e951ec09a631d
jsy8315 <tdhitbtd@gmail.com>
2024-05-28 22:41:53 +0900

    02.12.이벤트 버블링 응용과 dataset_이벤트버블링을 이용해 탭 방식 간단하게 구현

--- tab.js
+++ tab.js
@@ -1,7 +1,3 @@
-for (var i = 0; i < 3; i++) {
-    console.log(i);
-}
-
 // 버튼0 누르면 
 // - 모든 버튼에 붙은 orange 클래스명 제거
 // - 버튼0에 orange 클래스명 추가
@@ -14,13 +10,29 @@ var 컨텐트 = $('.tab-content');
 var countTab = $('.tab-content').length;
 
 // for 반복문으로 코드 복붙 
-for (let i = 0; i < countTab; i++) {
-    버튼.eq(i).on('click', function() {
-        버튼.removeClass('orange');
-        버튼.eq(i).addClass('orange');
-        컨텐트.removeClass('show');
-        컨텐트.eq(i).addClass('show');
-    })
+// for (let i = 0; i < countTab; i++) {
+//     버튼.eq(i).on('click', function() {
+//         탭열기(i)
+//     })
+// }
+$('.list').click(function(e){
+    if(e.target == document.querySelectorAll('.tab-button')[0]) {
+        탭열기(0)
+    } 
+    if(e.target == document.querySelectorAll('.tab-button')[1]) {
+        탭열기(1)
+    } 
+    if(e.target == document.querySelectorAll('.tab-button')[2]) {
+        탭열기(2)
+    }     
+
+})
+
+function 탭열기(숫자) {
+    버튼.removeClass('orange');
+    버튼.eq(숫자).addClass('orange');
+    컨텐트.removeClass('show');
+    컨텐트.eq(숫자).addClass('show');
 }
 
 // 버튼.eq(1).on('click', function() {
