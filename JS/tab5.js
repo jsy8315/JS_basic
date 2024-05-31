cdfab523fcc8f7588a3f8123b5573f653ff2e3ba
jsy8315 <tdhitbtd@gmail.com>
2024-05-31 11:49:13 +0900

    03.05_Select02 강의 시작 및 tab.js 정리

---
+++ JS/tab5.js
@@ -0,0 +1,39 @@
+// 좋은 관습 : 자주 쓰는 셀렉터 변수에 넣어쓰기
+var 버튼 = $('.tab-button');
+var 컨텐트 = $('.tab-content');
+var countTab = $('.tab-content').length;
+
+$('.list').click(function(e){
+    // 탭열기(지금 누른 버튼에 숨겨져있는 data-id) 
+    탭열기(parseInt(e.target.dataset.id))
+})
+
+function 탭열기(숫자) {
+    버튼.removeClass('orange');
+    버튼.eq(숫자).addClass('orange');
+    컨텐트.removeClass('show');
+    컨텐트.eq(숫자).addClass('show');
+}
+
+// 셔츠고르면 <select>하나 더 보여주기
+// 아오 어렵네요
+// 답안
+// 유저가 셔츠고르면 form-hide 떼어주세요
+var value = document.querySelectorAll('.form-select')[0].value;
+console.log(value);
+var selectedProducts02 = $('.form-select').eq(0).val();
+console.log(selectedProducts02);
+
+document.querySelectorAll('.form-select')[0].addEventListener('input', function(e) {
+    var value = e.currentTarget.value;
+    if (value == '셔츠') {
+        document.querySelectorAll('.form-select')[1].classList.remove('form-hide');
+    }
+    if (value == '모자') {
+        $('.form-select').eq(1).addClass('form-hide');
+    }
+});
+
+
+
+
