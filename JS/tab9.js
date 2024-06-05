487806e80611ad950a363df331a4d2259ff7feef
jsy8315 <tdhitbtd@gmail.com>
2024-06-05 11:34:32 +0900

    03.09_Ajax02.상품 더보기 버튼 만들기_쌩자바스크립트로 더보기 버튼누르면 데이터 가져오기 구현_내코드

--- JS/tab9.js
+++ JS/tab9.js
@@ -4,6 +4,22 @@ var products = [
     { id : 2, price : 60000, title : 'Black Monastery' }
   ];
 
+// 더보기 버튼 누르면 상품 3개 가져와서 출력하기
+// 버튼 누르기 - 클릭이벤트
+document.querySelectorAll('.btn-danger')[0].addEventListener('click', function(){
+
+  // 쌩자바스크립트로 구현하기
+fetch('https://codingapple1.github.io/js/more1.json')
+  .then(res => res.json())
+  .then(function(템플릿02){
+    console.log(템플릿02);
+  })
+  .catch(function(error){
+    console.log('실패ㅋㅋ');
+  })
+
+});
+
 // 내코드
 // products.forEach(function(a, i){
 //   var 템플릿 = `<div class="col-sm-4"><img src="https://via.placeholder.com/600" class="w-100"></div>`;
