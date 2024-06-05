6da76fc6aaa5f3fa4bddeb32da8c5fc67ded58ba
jsy8315 <tdhitbtd@gmail.com>
2024-06-05 14:04:44 +0900

    03.09_Ajax02.상품 더보기 버튼 만들기_더보기버튼 1번 누르기, 2번 누르기 구별

--- JS/tab9.js
+++ JS/tab9.js
@@ -5,24 +5,27 @@ var products = [
   ];
 
 // 더보기 버튼 누르면 상품 3개 가져와서 출력하기 - 쌩자바스크립트로 구현하기
-document.querySelectorAll('.btn-danger')[0].addEventListener('click', function(){
-fetch('https://codingapple1.github.io/js/more1.json')
-  .then(res => res.json())
-  .then(function(템플릿02){
-    console.log(템플릿02);
-  })
-  .catch(function(error){
-    console.log('실패ㅋㅋ');
-  })
+// document.querySelectorAll('.btn-danger')[0].addEventListener('click', function(){
+// fetch('https://codingapple1.github.io/js/more1.json')
+//   .then(res => res.json())
+//   .then(function(템플릿02){
+//     console.log(템플릿02);
+//   })
+//   .catch(function(error){
+//     console.log('실패ㅋㅋ');
+//   })
 
-});
+// });
 
 // 강사코드
+  // 누른횟수 카운팅
+var clickCnt = 0;
 $('.btn-danger').click(function(){
+  clickCnt += 1;
+  if (clickCnt == 1) {
+  // 1번이면 4,5,6번째 상품 보여주고
   $.get('https://codingapple1.github.io/js/more1.json')
   .done((data)=>{
-    console.log(data)
-
     data.forEach((a, i)=>{
       var 템플릿 = 
       `<div class="col-sm-4">
@@ -33,6 +36,25 @@ $('.btn-danger').click(function(){
       $('.row').append(템플릿)
     })
   })
+  } else if (clickCnt == 2) {
+  // 2번이면 7,8,9번째 상품 보여주기
+  $.get('https://codingapple1.github.io/js/more2.json')
+  .done((data02)=>{
+    data02.forEach((a, i)=>{
+      var 템플릿02 = 
+      `<div class="col-sm-4">
+        <img src="https://codingapple1.github.io/js/more2.json" class="w-100">
+        <h5>${data02[i].title}</h5>
+        <p>가격 : ${data02[i].price}</p>
+      </div>`;
+      $('.row').append(템플릿02)
+    })
+  })
+  } else {
+  // 3번이면 버튼 숨기기
+  alert("그만눌러라.")
+  }
+
 })
 
 
