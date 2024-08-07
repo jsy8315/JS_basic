b691beec8bce7ea3d632aacc67183fbf1e675582
jsy8315 <tdhitbtd@gmail.com>
2024-08-07 09:10:35 +0900

    Part02.10.탭기능 만들며 배우는 for 반복문2_복습01

--- JS/tab.js
+++ JS/tab.js
@@ -9,16 +9,17 @@ var 버튼 = $('.tab-button');
 var 컨텐트 = $('.tab-content');
 var countTab = $('.tab-content').length;
 
-// for 반복문으로 코드 복붙 
-// for (let i = 0; i < countTab; i++) {
-//     버튼.eq(i).on('click', function() {
-//         탭열기(i)
-//     })
-// }
-$('.list').click(function(e){
-    // 탭열기(지금 누른 버튼에 숨겨져있는 data-id) 
-    탭열기(parseInt(e.target.dataset.id))
-})
+//for 반복문으로 코드 복붙 
+for (let i = 0; i < countTab; i++) {
+    버튼.eq(i).on('click', function() {
+        탭열기(i)
+    })
+    console.log('맞는데 왜 안되지')
+}
+// $('.list').click(function(e){
+//     // 탭열기(지금 누른 버튼에 숨겨져있는 data-id) 
+//     탭열기(parseInt(e.target.dataset.id))
+// })
 
 function 탭열기(숫자) {
     버튼.removeClass('orange');
@@ -32,11 +33,4 @@ function 탭열기(숫자) {
 //     버튼.eq(1).addClass('orange');
 //     컨텐트.removeClass('show');
 //     컨텐트.eq(1).addClass('show');
-// })
-
-// 버튼.eq(2).on('click', function() {
-//     버튼.removeClass('orange');
-//     버튼.eq(2).addClass('orange');
-//     컨텐트.removeClass('show');
-//     컨텐트.eq(2).addClass('show');
 // })
